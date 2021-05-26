60aabcb0597e674dd6c669522dd97b7d110a61b7
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-05-26 23:38:22 +0900

    fix: getInstaller

--- functions/src/oauth.ts
+++ functions/src/oauth.ts
@@ -9,58 +9,62 @@ import { functions } from "./firebase/functions";
 import { Queue } from "./firebase/task";
 import { SendFirstMessageBody } from "./https";
 
-const installer = new InstallProvider({
-  clientId: CONFIG.slack.client_id,
-  clientSecret: CONFIG.slack.client_secret,
-  stateSecret: CONFIG.slack.state_secret,
-  authVersion: "v2",
-  installationStore: {
-    storeInstallation: async (_installation) => {
-      const installation = _installation as Installation<"v2", false>;
-      const teamId = installation.team.id;
-      const SlackOAuthDoc = await SlackOAuthDB.doc().get();
-      let data: FirestoreParams<SlackOAuth> = {};
-      if (SlackOAuthDoc.exists) {
-        data = {
-          installation,
-          updatedAt: FieldValue.serverTimestamp(),
-        };
-      } else {
-        data = {
-          installation,
-          createdAt: FieldValue.serverTimestamp(),
-          updatedAt: FieldValue.serverTimestamp(),
-        };
-      }
+const getInstaller = () => {
+  const installer = new InstallProvider({
+    clientId: CONFIG.slack.client_id,
+    clientSecret: CONFIG.slack.client_secret,
+    stateSecret: CONFIG.slack.state_secret,
+    authVersion: "v2",
+    installationStore: {
+      storeInstallation: async (_installation) => {
+        const installation = _installation as Installation<"v2", false>;
+        const teamId = installation.team.id;
+        const SlackOAuthDoc = await SlackOAuthDB.doc().get();
+        let data: FirestoreParams<SlackOAuth> = {};
+        if (SlackOAuthDoc.exists) {
+          data = {
+            installation,
+            updatedAt: FieldValue.serverTimestamp(),
+          };
+        } else {
+          data = {
+            installation,
+            createdAt: FieldValue.serverTimestamp(),
+            updatedAt: FieldValue.serverTimestamp(),
+          };
+        }
 
-      const tasksClient = new CloudTasksClient();
-      const body: SendFirstMessageBody = { teamId, userId: installation.user.id };
-      await tasksClient.createTask({
-        parent: tasksClient.queuePath(CONFIG.cloud_task.project, CONFIG.cloud_task.location, Queue.SendFirstMessage),
-        task: {
-          scheduleTime: {
-            seconds: dayjs().add(3, "second").unix(),
+        const tasksClient = new CloudTasksClient();
+        const body: SendFirstMessageBody = { teamId, userId: installation.user.id };
+        await tasksClient.createTask({
+          parent: tasksClient.queuePath(CONFIG.cloud_task.project, CONFIG.cloud_task.location, Queue.SendFirstMessage),
+          task: {
+            scheduleTime: {
+              seconds: dayjs().add(3, "second").unix(),
+            },
+            httpRequest: {
+              headers: { "Content-Type": "application/json" },
+              httpMethod: "POST",
+              url: `${CONFIG.cloud_task.base_url}/sendFirstMessageTask`,
+              body: toBase64(body),
+            },
           },
-          httpRequest: {
-            headers: { "Content-Type": "application/json" },
-            httpMethod: "POST",
-            url: `${CONFIG.cloud_task.base_url}/sendFirstMessageTask`,
-            body: toBase64(body),
-          },
-        },
-      });
-      await SlackOAuthDB.doc(teamId).set(data, { merge: true });
+        });
+        await SlackOAuthDB.doc(teamId).set(data, { merge: true });
+      },
+      fetchInstallation: async (_installQuery) => {
+        const installQuery = _installQuery as InstallationQuery<false>;
+        const SlackOAuthDoc = await SlackOAuthDB.doc(installQuery.teamId).get();
+        const data = SlackOAuthDoc.data() as SlackOAuth;
+        return data.installation;
+      },
     },
-    fetchInstallation: async (_installQuery) => {
-      const installQuery = _installQuery as InstallationQuery<false>;
-      const SlackOAuthDoc = await SlackOAuthDB.doc(installQuery.teamId).get();
-      const data = SlackOAuthDoc.data() as SlackOAuth;
-      return data.installation;
-    },
-  },
-});
+  });
+  return installer;
+};
 
 export const slackOAuthUrl = functions.https.onRequest(async (request, response) => {
+  const installer = getInstaller();
   const url = await installer.generateInstallUrl({
     scopes: [
       "channels:history",
@@ -80,5 +84,6 @@ export const slackOAuthUrl = functions.https.onRequest(async (request, response)
 });
 
 export const slackOAuthRedirect = functions.https.onRequest(async (request, response) => {
+  const installer = getInstaller();
   await installer.handleCallback(request, response);
 });
